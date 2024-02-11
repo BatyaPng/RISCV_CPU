4110df9249e4e12873b3984beb0125db98708927
Oleg <olegberezin2002@gmail.com>
2024-02-11 21:17:35 +0300

    comments added

--- src/AluDecoder.sv
+++ src/AluDecoder.sv
@@ -12,17 +12,17 @@ assign RtypeSub = funct7b5 & opb5;
 
 always_comb begin
     case (ALUOp)
-        2'b00: ALUControl = 3'b000; 
-        2'b01: ALUControl = 3'b001; 
-        default: case(funct3) I
+        2'b00: ALUControl = 3'b000;                     // addition
+        2'b01: ALUControl = 3'b001;                     // subtraction
+        default: case(funct3)                           // type R or I
                     3'b000: if (RtypeSub)
-                                ALUControl = 3'b001; 
+                                ALUControl = 3'b001;    // sub
                             else
-                                ALUControl = 3'b000; 
-                    3'b010: ALUControl = 3'b101; 
-                    3'b110: ALUControl = 3'b011; 
-                    3'b111: ALUControl = 3'b010; 
-                    default: ALUControl = 3'bxxx; 
+                                ALUControl = 3'b000;    // add, addi
+                    3'b010: ALUControl = 3'b101;        // slt, slti
+                    3'b110: ALUControl = 3'b011;        // or, ori
+                    3'b111: ALUControl = 3'b010;        // and, andi
+                    default: ALUControl = 3'bxxx;       // ub
                  endcase
     endcase
 end
