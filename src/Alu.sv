9048582ec2ce6fbfa5ecbc27257991e78b155497
Oleg <olegberezin2002@gmail.com>
2024-02-13 12:40:30 +0300

    alpha verson

--- src/Alu.sv
+++ src/Alu.sv
@@ -3,16 +3,16 @@ module alu (
     input [31:0] SrcA, SrcB,
 
     output Zero,
-    output [31:0] ALUResult
+    output reg [31:0] ALUResult
 );
 
 always_comb begin
     case (ALUControl)
-        3'b000: ALUResult <= SrcA + SrcB;            // Addition
-        3'b001: ALUResult <= SrcA - SrcB;            // Substraction
-        3'b010: ALUResult <= SrcA & SrcB;            // And
-        3'b011: ALUResult <= SrcA | SrcB;            // Or
-        3'b101: ALUResult <= 1 ? SrcA < SrcB : 0;    //SLT 
+        3'b000: ALUResult = SrcA + SrcB;            // Addition
+        3'b001: ALUResult = SmrcA - SrcB;            // Substraction
+        3'b010: ALUResult = SrcA & SrcB;            // And
+        3'b011: ALUResult = SrcA | SrcB;            // Or
+        3'b101: ALUResult = 1 ? SrcA < SrcB : 0;    //SLT 
     endcase
 end
 
--- test/Top.sv
+++ test/Top.sv
@@ -4,12 +4,13 @@ module top(input logic clk, reset,
 );
     
 logic [31:0] PC, Instr, ReadData;
+logic [31:0] ReadData2;
 
 cpu cpu(clk, reset, PC, Instr, 
                      MemWrite, DataAdr, 
                      WriteData, ReadData);
 
 imem imem(PC, Instr);
-dmem dmem(clk, MemWrite, DataAdr, WriteData, ReadData);
+dmem dmem(clk, MemWrite, DataAdr, WriteData, ReadData2);
 
 endmodule
