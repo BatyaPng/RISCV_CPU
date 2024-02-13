d206afef11ab603ac273c5425fdcd82624b8ea79
Oleg <olegberezin2002@gmail.com>
2024-02-13 13:29:12 +0300

    refactoring

--- src/Alu.sv
+++ src/Alu.sv
@@ -9,7 +9,7 @@ module alu (
 always_comb begin
     case (ALUControl)
         3'b000: ALUResult = SrcA + SrcB;            // Addition
-        3'b001: ALUResult = SmrcA - SrcB;            // Substraction
+        3'b001: ALUResult = SrcA - SrcB;            // Substraction
         3'b010: ALUResult = SrcA & SrcB;            // And
         3'b011: ALUResult = SrcA | SrcB;            // Or
         3'b101: ALUResult = 1 ? SrcA < SrcB : 0;    //SLT 
